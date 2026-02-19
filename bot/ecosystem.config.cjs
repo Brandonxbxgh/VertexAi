9797f1048117667175a4cb90d94f76f5543ba286
Brandonxbxgh <bahobgood01@gmail.com>
2026-02-18 20:38:23 -0600

    Fix PM2: use fork mode instead of cluster

--- bot/ecosystem.config.cjs
+++ bot/ecosystem.config.cjs
@@ -12,6 +12,7 @@ module.exports = {
       cwd: __dirname,
       node_args: "-r dotenv/config",
       instances: 1,
+      exec_mode: "fork",
       autorestart: true,
       watch: false,
       max_memory_restart: "500M",
